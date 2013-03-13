d5dad0cf02962a6e659f5736445a41f6b1afd933
Philipp Neugebauer <flippus@nexgo.de>
2013-03-13 15:52:16 +0100

    database connection and filling in works now

--- DTM_Assignment_1/src/it/unibz/dtm/assignment1/flp/DatabaseConnection.java
+++ DTM_Assignment_1/src/it/unibz/dtm/assignment1/flp/DatabaseConnection.java
@@ -26,12 +26,13 @@ public class DatabaseConnection {
 		File publFile = new File(
 				"src/it/unibz/dtm/assignment1/flp/data/publ.tsv");
 		File[] fileArray = { authFile, publFile };
-		String[] queryArray = { "Auth(name, pubID) VALUES(?, ?)",
-				"Publ(pubID, type, title, booktitle, year, publisher) VALUES(?, ?, ?, ?, ?, ?)" };
+		String[] queryArray = { "auth(name, pubid) VALUES(?, ?)",
+				"publ(pubid, type, title, booktitle, year, publisher) VALUES(?, ?, ?, ?, ?, ?)" };
 		int[] attributeNumber = { 2, 6 };
 
 		try {
 			for (int i = 0; i < fileArray.length; i++) {
+				System.out.println("i " + i);
 				reader = new CSVReader(new FileReader(fileArray[i]), '\t');
 				c = DriverManager.getConnection(url, user, password);
 				String[] line;
@@ -41,6 +42,18 @@ public class DatabaseConnection {
 							.prepareStatement(insertQuery);
 					for (int j = 0; j < attributeNumber[i]; j++) {
 						statement.setString(j + 1, line[j]);
+					}
+					if (c.isValid(10)) {
+					} else {
+						try {
+							if (c != null) {
+								c.close();
+							}
+							c = DriverManager
+									.getConnection(url, user, password);
+						} catch (SQLException ex) {
+							System.out.println(ex.getMessage());
+						}
 					}
 					statement.executeUpdate();
 				}
@@ -59,5 +72,6 @@ public class DatabaseConnection {
 				System.out.println(ex.getMessage());
 			}
 		}
+		System.out.println("finished");
 	}
 }
