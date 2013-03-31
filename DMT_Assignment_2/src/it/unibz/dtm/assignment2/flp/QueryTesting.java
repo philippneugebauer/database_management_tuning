d6a49a008198324cba2201bdbdf0654f8eb7eb4a
Philipp Neugebauer <flippus@nexgo.de>
2013-03-31 21:41:35 +0200

    improved code

--- DMT_Assignment_2/src/it/unibz/dtm/assignment2/flp/QueryTesting.java
+++ DMT_Assignment_2/src/it/unibz/dtm/assignment2/flp/QueryTesting.java
@@ -2,7 +2,6 @@ package it.unibz.dtm.assignment2.flp;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -23,41 +22,37 @@ public class QueryTesting {
 			c = DriverManager.getConnection(url, user, password);
 			Statement s = c.createStatement();
 			startTime = DateTime.now();
-			ResultSet results = s
-					.executeQuery("SELECT AVG(salary) FROM employee WHERE dept IN (SELECT dept FROM techdept)");
+			s.executeQuery("SELECT AVG(salary) FROM employee WHERE dept IN (SELECT dept FROM techdept)");
 			endTime = DateTime.now();
 			System.out.println("seconds duration "
 					+ (endTime.getMillis() - startTime.getMillis()) / 1000);
 
-			while (results.next()) {
-				int i = 0;
-				System.out.println(results.getObject(i));
-				i++;
-			}
-
+			// TODO: check, ob wirklich statement tuning
 			s = c.createStatement();
 			startTime = DateTime.now();
-			results = s
-					.executeQuery("SELECT AVG(salary) FROM Employee, Techdept WHERE Employee.dept = Techdept.dept");
+			s.executeQuery("SELECT AVG(salary) FROM Employee, Techdept WHERE Employee.dept = Techdept.dept");
 			endTime = DateTime.now();
 			System.out.println("seconds duration "
 					+ (endTime.getMillis() - startTime.getMillis()) / 1000);
 
-			s = c.createStatement();
-			startTime = DateTime.now();
-			results = s
-					.executeQuery("SELECT ssnum FROM Employee e1, Techdept WHERE salary = (SELECT AVG(e2.salary) FROM Employee e2, Techdept WHERE e2.dept = e1.dept AND e2.dept = Techdept.dept)");
-			endTime = DateTime.now();
-			System.out.println("seconds duration "
-					+ (endTime.getMillis() - startTime.getMillis()) / 1000);
+			// FIXME: dauert viel zu lang oO
+			// s = c.createStatement();
+			// startTime = DateTime.now();
+			// results = s
+			// .executeQuery("SELECT ssnum FROM Employee e1, Techdept WHERE salary = (SELECT AVG(e2.salary) FROM Employee e2, Techdept WHERE e2.dept = e1.dept AND e2.dept = Techdept.dept)");
+			// endTime = DateTime.now();
+			// System.out.println("seconds duration "
+			// + (endTime.getMillis() - startTime.getMillis()) / 1000);
 
 			s = c.createStatement();
 			startTime = DateTime.now();
-			s.executeQuery("SELECT AVG(salary) as avsalary, Employee.dept INTO Temp FROM Employee, Techdept WHERE Employee.dept = Techdept.dept GROUP BY Employee.dept");
+			s.executeUpdate("SELECT AVG(salary) as avsalary, Employee.dept INTO Temp FROM Employee, Techdept WHERE Employee.dept = Techdept.dept GROUP BY Employee.dept");
 			s.executeQuery("SELECT ssnum FROM Employee, Temp WHERE salary = avsalary AND Employee.dept = Temp.dept");
 			endTime = DateTime.now();
 			System.out.println("seconds duration "
 					+ (endTime.getMillis() - startTime.getMillis()) / 1000);
+			s = c.createStatement();
+			s.executeUpdate("DROP Table temp");
 
 		} catch (SQLException ex) {
 			System.out.println(ex.getMessage());
