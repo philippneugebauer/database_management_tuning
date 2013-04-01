d5f6b5f0213cdf81b3788d3b64b95569663f8527
Philipp Neugebauer <flippus@nexgo.de>
2013-04-01 02:35:35 +0200

    milliseconds of the time will be shown now

--- DMT_Assignment_2/src/it/unibz/dtm/assignment2/flp/QueryTesting.java
+++ DMT_Assignment_2/src/it/unibz/dtm/assignment2/flp/QueryTesting.java
@@ -25,26 +25,30 @@ public class QueryTesting {
 			s.executeQuery("SELECT AVG(salary) FROM employee WHERE dept IN (SELECT dept FROM techdept)");
 			endTime = DateTime.now();
 			System.out.println("seconds duration "
-					+ (endTime.getMillis() - startTime.getMillis()) / 1000);
+					+ Double.valueOf(endTime.getMillis()
+							- startTime.getMillis()) / 1000);
 
 			startTime = DateTime.now();
 			s.executeQuery("SELECT AVG(salary) FROM Employee, Techdept WHERE Employee.dept = Techdept.dept");
 			endTime = DateTime.now();
 			System.out.println("seconds duration "
-					+ (endTime.getMillis() - startTime.getMillis()) / 1000);
+					+ Double.valueOf(endTime.getMillis()
+							- startTime.getMillis()) / 1000);
 
 			startTime = DateTime.now();
 			s.executeQuery("SELECT ssnum FROM employee e1 WHERE salary = (SELECT AVG(e2.salary) FROM employee e2, techdept WHERE e2.dept = e1.dept AND e2.dept = techdept.dept)");
 			endTime = DateTime.now();
 			System.out.println("seconds duration "
-					+ (endTime.getMillis() - startTime.getMillis()) / 1000);
+					+ Double.valueOf(endTime.getMillis()
+							- startTime.getMillis()) / 1000);
 
 			startTime = DateTime.now();
 			s.executeUpdate("SELECT AVG(salary) as avsalary, employee.dept INTO TEMPORARY TABLE temp FROM employee, techdept WHERE employee.dept = techdept.dept GROUP BY employee.dept");
 			s.executeQuery("SELECT ssnum FROM employee, temp WHERE salary = avsalary AND employee.dept = temp.dept");
 			endTime = DateTime.now();
 			System.out.println("seconds duration "
-					+ (endTime.getMillis() - startTime.getMillis()) / 1000);
+					+ Double.valueOf(endTime.getMillis()
+							- startTime.getMillis()) / 1000);
 
 		} catch (SQLException ex) {
 			System.out.println(ex.getMessage());
