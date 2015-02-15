fdb8d437c33820d06d4fdf892925792c7654d0eb
Philipp Neugebauer <ppn@philippneugebauer.net>
2015-02-15 16:10:53 +0100

    restructured project to gradle fittings, assignment 2

---
+++ DMT_Assignment_2/src/main/java/it/unibz/dtm/assignment2/flp/QueryTesting.java
@@ -0,0 +1,65 @@
+package it.unibz.dtm.assignment2.flp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.joda.time.DateTime;
+
+public class QueryTesting {
+
+    public static void main(String args[]) {
+
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "postgres";
+        String password = "test";
+        Connection c = null;
+
+        DateTime startTime;
+        DateTime endTime;
+        try {
+            c = DriverManager.getConnection(url, user, password);
+            Statement s = c.createStatement();
+            startTime = DateTime.now();
+            s.executeQuery("SELECT AVG(salary) FROM employee WHERE dept IN (SELECT dept FROM techdept)");
+            endTime = DateTime.now();
+            System.out.println("seconds duration "
+                    + Double.valueOf(endTime.getMillis()
+                            - startTime.getMillis()) / 1000);
+
+            startTime = DateTime.now();
+            s.executeQuery("SELECT AVG(salary) FROM Employee, Techdept WHERE Employee.dept = Techdept.dept");
+            endTime = DateTime.now();
+            System.out.println("seconds duration "
+                    + Double.valueOf(endTime.getMillis()
+                            - startTime.getMillis()) / 1000);
+
+            startTime = DateTime.now();
+            s.executeQuery("SELECT ssnum FROM employee e1 WHERE salary = (SELECT AVG(e2.salary) FROM employee e2, techdept WHERE e2.dept = e1.dept AND e2.dept = techdept.dept)");
+            endTime = DateTime.now();
+            System.out.println("seconds duration "
+                    + Double.valueOf(endTime.getMillis()
+                            - startTime.getMillis()) / 1000);
+
+            startTime = DateTime.now();
+            s.executeUpdate("SELECT AVG(salary) as avsalary, employee.dept INTO TEMPORARY TABLE temp FROM employee, techdept WHERE employee.dept = techdept.dept GROUP BY employee.dept");
+            s.executeQuery("SELECT ssnum FROM employee, temp WHERE salary = avsalary AND employee.dept = temp.dept");
+            endTime = DateTime.now();
+            System.out.println("seconds duration "
+                    + Double.valueOf(endTime.getMillis()
+                            - startTime.getMillis()) / 1000);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                if (c != null) {
+                    c.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
